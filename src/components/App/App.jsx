14f214273bb9cf1974fe4e536b4fc1af4ae0f05a
DabirGomezSerna <dasego.gomezserna@gmail.com>
2025-12-04 12:41:56 -0600

    Added ProductDetails page and associated route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import { CartProvider } from "../../context/CartContext";
 import Home from "../../pages/Home/Home";
 import Layout from "../../layout/Layout";
 import "./App.css";
+import Product from "../../pages/Product";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/product/:productId" element={<Product />}/>
             <Route path="*" element={<div>Page not available</div>} />
           </Routes>
         </Layout>
