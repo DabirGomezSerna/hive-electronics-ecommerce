a29dd7aec12b81e774b278ff8644455b86e2153b
DabirGomezSerna <dasego.gomezserna@gmail.com>
2025-12-09 16:26:05 -0600

    Added checkout, order, login and protected routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,6 +5,10 @@ import Layout from "../../layout/Layout";
 import Product from "../../pages/Product";
 import CategoryPage from "../../pages/CategoryPage";
 import Cart from "../../pages/Cart/Cart";
+import Checkout from "../../pages/Checkout/Checkout";
+import Order from "../../pages/Order/Order";
+import Login from "../../pages/Login/Login";
+import ProtectedRoute from "../../pages/ProtectedRoute";
 import "./App.css";
 
 export default function App() {
@@ -15,8 +19,25 @@ export default function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/cart" element={<Cart />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/product/:productId" element={<Product />} />
             <Route path="/category/:categoryId" element={<CategoryPage />} />
+            <Route
+              path="/checkout"
+              element={
+                <ProtectedRoute>
+                  <Checkout />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/order-confirmation"
+              element={
+                <ProtectedRoute>
+                  <Order />
+                </ProtectedRoute>
+              }
+            />
             <Route path="*" element={<div>Page not available</div>} />
           </Routes>
         </Layout>
