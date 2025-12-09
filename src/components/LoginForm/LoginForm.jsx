36e61ffdaf03a3b467cdf6dfb0d82ff9827f2eea
DabirGomezSerna <dasego.gomezserna@gmail.com>
2025-12-09 16:27:41 -0600

    Created LoginForm component

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { login } from "../../services/userServices";
+import Button from "../common/Button/Button";
+import ErrorMessage from "../common/ErrorMessage/ErrorMessage";
+import Input from "../common/Input/Input";
+import "./LoginForm.css";
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError("");
+
+    await new Promise((resolve) => setTimeout(resolve, 800));
+    const result = await login(email, password);
+
+    if (result.success) {
+      navigate("/");
+      window.location.reload();
+    } else {
+      setError(result.error);
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        <h2>Log in</h2>
+        <div className="demo-users">
+          <h4>Demo users:</h4>
+          <div className="user-demo">
+            <strong>John Doe:</strong> john@email.com / john123
+          </div>
+          <div className="user-demo">
+            <strong>Jane Doe:</strong> jane@email.com / jane123
+          </div>
+        </div>
+        <form className="login-form" onSubmit={onSubmit}>
+          <div className="form-group">
+            <Input
+              id="email"
+              label="Enter your email: "
+              type="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              placeholder="Email"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <Input
+              id="password"
+              label="Enter your password: "
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              placeholder="Password"
+              required
+            />
+          </div>
+
+          {error && <ErrorMessage>{error}</ErrorMessage>}
+
+          <Button disabled={loading} type="submit" variant="primary">
+            {loading ? "Logging in..." : "Log in"}
+          </Button>
+        </form>
+        <div className="login-footer">
+          <Button className="login-footer-btn" variant="secondary" size="md" onClick={()=>navigate("/")}>Back to home</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
